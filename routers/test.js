2802dd2ac9887cee4e9fee3ae3517a19e100fadc
ragavanperarasu <ragavanperarasu5761@gmail.com>
2025-12-18 21:34:29 +0530

    modified:   routers/test.js

--- routers/test.js
+++ routers/test.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // A simple test route
 router.get('/test', (req, res) => {
+    console.log("Test route accessed");
   res.json({ message: 'Test route is working!' });
 });
 
